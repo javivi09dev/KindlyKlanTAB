12da48e7e619d51d84c990bfeb3766561ef0dcc0
Javier <javierotoalonso@gmail.com>
2025-06-05 19:37:28 +0200

    init project

---
+++ src/main/java/me/javivi/kktab/Kindlyklantab.java
@@ -0,0 +1,10 @@
+package me.javivi.kktab;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Kindlyklantab implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
